5df15187cf8d082acc8d1269a4fc85e9dd32df78
Linus Nagel <129159411+0qln@users.noreply.github.com>
2023-07-21 01:19:32 +0200

    Initial commit

---
+++ Extension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p4KFinder
+{
+    internal static class Extension
+    {
+        public static void AddRange<T>(this HashSet<T> set, IEnumerable<T> values)
+        {
+            foreach(var item in values)
+            {
+                set.Add(item);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using p4KFinder;
+
+
+Console.WriteLine("Enter your directory: ");
+
+string parentDir = Console.ReadLine() ?? "";
+if (!Directory.Exists(parentDir)) return;
+
+int dupFileNames = 0;
+
+string newDir = parentDir + $"\\pseudo4K_";
+
+List<string> list = GetAllPseudo4kImages(parentDir).ToList();
+
+Directory.CreateDirectory(newDir);
+
+
+foreach (var img in list)
+{
+    string linkName = newDir + "\\" + Path.GetFileName(img) + "_LINK";
+    int ctr = 0;
+    while (File.Exists(NewName(linkName, ctr)))
+    {
+        ctr++;
+        dupFileNames++;
+    }
+    File.CreateSymbolicLink(NewName(linkName, ctr), img);
+
+    Console.WriteLine($"Link created: {linkName}");
+}
+
+Console.WriteLine($"Finished. Created {list.Count()} links. Duplicate file names: {dupFileNames}");
+
+string NewName(string name, int ctr)
+{
+    return name + $" ({ctr})";
+}
+
+
+
+List<string> GetAllFiles(string directory)
+{
+    var result = new List<string>();
+
+    foreach (var file in Directory.GetFiles(directory))
+        if (!result.Contains(file))
+            result.Add(file);
+
+    foreach (var dir in Directory.GetDirectories(directory))
+    {
+        var data = GetAllFiles(dir);
+        foreach (var file in data)
+            if (!result.Contains(file))
+                result.Add(file);
+    }
+    return result;
+}
+
+IEnumerable<string> GetAllPseudo4kImages(string dir)
+{
+    var result = new List<string>();
+
+    foreach (string path in Directory.GetFiles(dir))
+    {
+        try
+        {
+            using (var imageStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var data = Image.FromStream(imageStream, false, false))
+            {
+                if (data.Width > 1920 && data.Height > 1920)
+                {
+                    result.Add(path);
+                    Console.WriteLine($"Image found: {path}");
+                }
+            }
+        }
+        catch { }
+    }
+
+    foreach (string path in Directory.GetDirectories(dir))
+    {
+        result.AddRange(GetAllPseudo4kImages(path));
+    }
+
+    return result;
+}
+
+
+
+
